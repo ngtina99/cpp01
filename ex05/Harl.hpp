bb00965caeb113d71d3b14f2b9224454c8d15b13
Tina <nguyen.valentina.99@gmail.com>
2024-10-03 19:42:55 +0200

    update

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -1,38 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thuy-ngu <thuy-ngu@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/30 07:03:24 by thuy-ngu          #+#    #+#             */
-/*   Updated: 2024/09/30 07:03:24 by thuy-ngu         ###   ########.fr       */
+/*   Created: 2024/10/03 19:41:46 by thuy-ngu          #+#    #+#             */
+/*   Updated: 2024/10/03 19:41:46 by thuy-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanA.hpp"
-#include "HumanB.hpp"
-
-int main()
-{
-	{
-		Weapon club = Weapon("crude spiked club");
-
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("crude spiked club");
-
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
+#ifndef HARL_HPP
+# define HARL_HPP
 
 
-	return 0;
-}
+
+#endif
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -5,34 +5,10 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thuy-ngu <thuy-ngu@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/30 07:03:24 by thuy-ngu          #+#    #+#             */
-/*   Updated: 2024/09/30 07:03:24 by thuy-ngu         ###   ########.fr       */
+/*   Created: 2024/10/03 19:42:33 by thuy-ngu          #+#    #+#             */
+/*   Updated: 2024/10/03 19:42:33 by thuy-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanA.hpp"
-#include "HumanB.hpp"
-
-int main()
-{
-	{
-		Weapon club = Weapon("crude spiked club");
-
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("crude spiked club");
-
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
-
-
-	return 0;
-}
+int	main()
+{}
